4c47c3afe1f62e367eae54ba27f6986fb2d25b8f
Lucas M Uriarte <lucasmuriarte@gmail.com>
2020-11-16 12:36:33 +0100

    adding more detaial and comment to threading example

--- example threading.py
+++ example threading.py
@@ -15,11 +15,12 @@ from  PyQt5.QtCore  import QThread, QObject, pyqtSignal, QPoint, QSize, QEvent, 
 import time
 
 
-#■option 1
+#option 1
 class GenericWorker(QObject):
 
-    start = pyqtSignal(str)
+    start = pyqtSignal(str)#create signals as the object does not have them
     finished = pyqtSignal()
+#    finished = pyqtSignal(int)
 
     def __init__(self, function, *args, **kwargs):
         super(GenericWorker, self).__init__()
@@ -37,7 +38,8 @@ class GenericWorker(QObject):
         print('start')
         self.isRunning=True
         self.function(*self.args, **self.kwargs)
-        self.finished.emit()
+        self.finished.emit()#emit the signal 
+        #self.finished.emit(value) #emit the signal with a value that can be used later on
         self.isRunning=False
 
 #option 2 run an specific function
@@ -50,9 +52,11 @@ class GenericWorker(QObject):
 #    
 #    @pyqtSlot()#maybe is not need here
 #    def run(self):
-#        longCalculation(self.time)
-#        
-
+#        longCalculation(self.time//2)
+#        self.iter_num.emit(1)#signal we have reach half of calculation
+#         longCalculation(self.time//2)
+        
+        
 # same as option 2 
 class ThreadMessage(QThread): 
     def __init__(self, parent=None):
@@ -90,7 +94,7 @@ class ModelWindow(QMainWindow):
         self.exit.clicked.connect(self.salir)
         self.progressBar.setGeometry(85,60+30,433,15)
         self.progressBar.setVisible(False)
-        self.thread = QThread() #¸create a thread
+        self.thread = QThread() #¸create a thread No need for option 2
         self.thread.start(QThread.TimeCriticalPriority) #start and define priority in the computer a thread
         self.exit.setGeometry(85,60+60,200,25)
         self.start.setGeometry(285,60+60,200,25)
@@ -104,13 +108,13 @@ class ModelWindow(QMainWindow):
         self.progressBar.show()
         my_worker = GenericWorker(longCalculation,number)
         my_worker.moveToThread(self.thread)
-        my_worker.start.emit("hello")  
+        my_worker.start.emit("hello") #calling starting signal will make my_worker.run() but we need to pass the signal
         my_worker.finished.connect(self.finished)
 
         #option 2
 #        this thread is kill after calculation is finish
 #        my_worker=WorkingThread(number)
-#        my_worker.start()
+#        my_worker.start() # make my_worker.run No need to pass the signal
 #        my_worker.finished.connect(self.finished)
         
     def closeEvent(self, event):
@@ -131,14 +135,9 @@ class ModelWindow(QMainWindow):
     def salir(self):
         self.close()
 
-#class Model:
-#
-#    def manualModelBuild(self):
+
 app = QApplication([])
 ex = ModelWindow()
 ex.show()
 app.exec()
-
-    
-#test111 = Model()
-#test111.manualModelBuild()    
+  
