04d7c9cef1043c16c633125bc41a195f029b35a7
luc labarrière <luc.labarriere@univ-lille.fr>
2021-02-19 19:27:44 +0100

    Made 3 new graphics examples

---
+++ examples/graphics/select_traces_graph.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from ultrafast.utils.divers import read_data, select_traces
+from ultrafast.experiment import Experiment
+import matplotlib.pyplot as plt
+
+path = 'examples/data/denoised_2.csv'
+time, data, wave = read_data(path, wave_is_row=True)
+
+exp = Experiment(time, data, wave)
+
+exp.select_traces()
+exp.initialize_exp_params(0, None, 4, 40, 500)
+exp.global_fit()
+
+exp.select_traces_graph()
+
+plt.show()
