a532ec88d6e18909c8cbde2db2d40700c564ef09
Stahux <sn@nizin.pl>
2021-09-26 15:30:43 +0200

    Working code with test of dataset generator and DAS fitting, but some problem with convergence occurs.

--- tests/integrity_tests/test_DAS_fitting.py
+++ tests/integrity_tests/test_DAS_fitting.py
@@ -8,6 +8,10 @@ Created on Sat Sep 25 15:37:51 2021
 
 import unittest
 from ultrafast.utils.divers import DataSetCreator
+from ultrafast.fit.GlobalFit import GlobalFitExponential
+from ultrafast.utils.divers import read_data, select_traces
+from ultrafast.graphics.ExploreResults import ExploreResults
+from ultrafast.fit.GlobalParams import GlobExpParameters
 
 #datasets_dir = "ultrafast/examples/dynamically_created_data/"
 
@@ -24,52 +28,59 @@ class TestDatasetsDAS(unittest.TestCase):
         self.datasets_dir = "../../examples/dynamically_created_data/"
         
     
-    def helperFunc(self):
-        return 0
-    
-    def test_genAndFit1exp1bandDAS(self):
+    def test_genAndFit3expNoConvNoNoiseDAS(self):
         #generate and save dataset, then fit it and verify results
         
-        wave = DataSetCreator.generate_wavelength(400,700,500)
+        wave = DataSetCreator.generate_wavelength(400,700,31)
         peaks=[[470,550,650],[480,700],[500,600]]
         amplitudes=[[-10,-6,12],[-9,11],[-8,6]]
         fwhms=[[50,100,80],[40,70],[30,65]]
-        shapes = DataSetCreator.generate_specific_shape(wave, taus=3, peaks=peaks, amplitudes=amplitudes, fwhms=fwhms)
+        shapes = DataSetCreator.generate_specific_shape(wave, taus=3, 
+                                                        peaks=peaks, 
+                                                        amplitudes=amplitudes, 
+                                                        fwhms=fwhms)
         k1=1/5.0
         k2=1/20.0
         k3=1/100.0
-        kmatrix = [[-k1,0,0],[k1/3,-k2,0],[2*k1/3,0,-k3]]
-        initials = [1.0,0,0]
+        kmatrix = [[-k1,0,0],[0,-k2,0],[0,0,-k3]]
+        initials = [1.0,1.0,1.0]
         profiles = DataSetCreator.generate_profiles(500.0,5000,initials,kmatrix)
-        data_set_conv = DataSetCreator.generate_dataset(shapes, profiles, 1.2)
-        new_times = DataSetCreator.generate_time(data_set_conv.index[0],data_set_conv.index[-1],120)
+        data_set_conv = DataSetCreator.generate_dataset(shapes, profiles, 0.2)
+        new_times = DataSetCreator.generate_time(data_set_conv.index[0],data_set_conv.index[-1]-0.01,120)
         data_set_conv_proj = DataSetCreator.timegrid_projection(data_set_conv, new_times)        
         
-        data_set_conv_proj.to_csv(self.datasets_dir+"DAS_1exp_1band_test1.csv")
+        datapath = self.datasets_dir+"DAS_3exp_noconv_nonoise_test1.csv"
+        
+        data_set_conv_proj.to_csv(datapath)
+        
+        time, data, wavelength = read_data(datapath, wave_is_row = True)
+        
+        data_select, wave_select = select_traces(data, wavelength, 10)
+        params = GlobExpParameters(data_select.shape[1], [5, 20, 100])
+        params.adjustParams(0, False, fwhm=0.2, opt_fwhm=True)
+        parameters = params.params
+        
+        fitter = GlobalFitExponential(time, data_select, 3, parameters, True,
+                                      wavelength=wave_select)
+        
+        fitter.allow_stop = False #in my case it just hangs.
+        result = fitter.global_fit(maxfev=10000)
+        
+        explorer = ExploreResults(result)
+        explorer.print_results()
+        explorer.plot_fit()
+        explorer.plot_DAS()
+        
+        
+  
+        
+        self.assertTrue(True,msg="Cannot fit dynamically generated data!")   
         
         
         
         
         
-        
-        
-        
-        self.assertTrue(True,msg="Shit fucked up.")   
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
         
         
 if __name__ == '__main__':
