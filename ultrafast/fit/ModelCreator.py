Ok, one error corrected. But i found lmfit does not treat fixed/constrained params as real params and does not pass them to scipy, but leaves jacobian unchanged. It will have to be resolved, but it may be also useful to propose some fix for the lmfit itself.

ultrafast/fit/ModelCreator.py
@@ -269,7 +269,7 @@ class ModelCreator:
         1darray of size equal to time-vector 
         """    
         
-        return np.ones(self.x.shape(0))
+        return np.ones(self.x.shape[0])
     
     def expNGaussDatasetJacobianByYInf(self, params, lambda_i, tau_j):
         """