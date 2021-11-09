3aa02c3620bf8138324e645f3da2e7c187aecb92
luc labarrière <luc.labarriere@univ-lille.fr>
2021-11-09 20:49:16 +0100

    Working test_GlobalFitClass

--- tests/fit/test_GlobalFitBootstrap.py
+++ tests/fit/test_GlobalFitBootstrap.py
@@ -1,87 +1,101 @@
 import unittest
-import pickle
+import pickle, os
 from ultrafast.fit.GlobalFitBootstrap import BootStrap
 from parameterized import parameterized
 from ultrafast.old.ExponentialFit import GlobalFitExponential
 from ultrafast.utils.Preprocessing import ExperimentException
 from copy import deepcopy
-
-path = 'tests/ultrafast/fit/test_result.obj'
-
-with open(path, 'rb') as file:
-    result = pickle.load(file)
-
-boot = BootStrap(result)
-data = result.data
+from ultrafast.utils.divers import get_root_directory
 
 
 class TestBootStrap(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+
+        path = os.path.join(
+            get_root_directory(),
+            'tests/fit/test_result.obj'
+        )
+
+        with open(path, 'rb') as file:
+            cls.result = pickle.load(file)
+
+        cls.boot = BootStrap(cls.result)
+        cls.data = cls.result.data
+        return super().setUpClass()
 
     @parameterized.expand([[10], [7], [15]])
-    def test__data_sets_from_data(self, n_boots):
-        res = boot._data_sets_from_data(n_boots)
-        self.assertEqual(data.shape, res[:, :, 1].shape)
+    def test_data_sets_from_data(self, n_boots):
+        res = self.boot._data_sets_from_data(n_boots)
+        self.assertEqual(self.data.shape, res[:, :, 1].shape)
         self.assertEqual(n_boots, res.shape[2])
     
     @parameterized.expand([[10], [7], [15]])
-    def test__data_sets_from_residues(self, n_boots):
-        res = boot._data_sets_from_residues(n_boots)
-        self.assertEqual(data.shape, res[:, :, 1].shape)
+    def test_data_sets_from_residues(self, n_boots):
+        res = self.boot._data_sets_from_residues(n_boots)
+        self.assertEqual(self.data.shape, res[:, :, 1].shape)
         self.assertEqual(n_boots, res.shape[2])
 
-    def test__get_original_fitter(self):
-        params2 = deepcopy(result.estimation_params)
+    def test_get_original_fitter(self):
+        params2 = deepcopy(self.result.estimation_params)
         for i in params2:
             params2[i].value = params2[i].init_value
-        fitter, params = boot._get_original_fitter()
+        fitter, params = self.boot._get_original_fitter()
         self.assertEqual(fitter, GlobalFitExponential)
         self.assertEqual(params,  params2)
 
-    def test__details(self):
-        exp_no, type_fit, deconv, maxfev, tau_inf = boot._details()
-        self.assertEqual(result.details['exp_no'], exp_no)
-        self.assertEqual(result.details['type'], type_fit)
-        self.assertEqual(result.details['deconv'], deconv)
-        self.assertEqual(result.details['maxfev'], maxfev)
-        self.assertEqual(result.details['tau_inf'], tau_inf)
+    def test_details(self):
+        exp_no, type_fit, deconv, maxfev, tau_inf = self.boot._details()
+        self.assertEqual(self.result.details['exp_no'], exp_no)
+        self.assertEqual(self.result.details['type'], type_fit)
+        self.assertEqual(self.result.details['deconv'], deconv)
+        self.assertEqual(self.result.details['maxfev'], maxfev)
+        self.assertEqual(self.result.details['tau_inf'], tau_inf)
 
     @parameterized.expand([[10], [15], [20], [25], [33], [50], [14], [38]])
-    def test__get_division_number(self, val):
+    def test_get_division_number(self, val):
         if val in [10, 15, 20, 25, 33, 50]:
-            div = boot._get_division_number(val)
+            div = self.boot._get_division_number(val)
+
             self.assertEqual(div, round(100/val))
+            
         else:
             with self.assertRaises(ExperimentException) as context:
-                boot._get_division_number(val)
+                self.boot._get_division_number(val)
             msg = 'Size should be either 10, 15, 20, 25, 33, this is ' \
                   'the values in percentage that will be randomly changed'
+
             self.assertTrue(msg in str(context.exception))
 
-    def test__get_fit_params_names(self):
-        exp_no, type_fit, deconv, maxfev, tau_inf = boot._details()
-        names = boot._get_fit_params_names(type_fit, exp_no, deconv)
+    def test_get_fit_params_names(self):
+        exp_no, type_fit, deconv, maxfev, tau_inf = self.boot._details()
+        names = self.boot._get_fit_params_names(type_fit, exp_no, deconv)
+
         self.assertEqual(names, ['t0_1', 'tau1_1', 'tau2_1', 'tau3_1'])
     
-    def test__generate_pandas_results_dataframe(self):
-        pd_frame = boot._generate_pandas_results_dataframe()
+    def test_generate_pandas_results_dataframe(self):
+        pd_frame = self.boot._generate_pandas_results_dataframe()
         pd_keys = [i for i in pd_frame.keys()]
         df = ['N° Iterations', 'red Xi^2', 'success']
         suposed_keys = ['tau1 initial', 'tau1 final', 'tau2 initial', 
                         'tau2 final', 'tau3 initial', 'tau3 final'] + df
+
         self.assertEqual(pd_keys, suposed_keys)
     
     @parameterized.expand([[['t0_1', 'tau1_1', 'tau2_1', 'tau3_1'], True],
                            [['t0_1', 'tau1_1', 'tau3_1'], False],
                            [['t0_1', 'tau3_1'], True]])
-    def test__initial_final_values(self, names, only_vary):
-        params = result.estimation_params
+    def test_initial_final_values(self, names, only_vary):
+        params = self.result.estimation_params
         initial_values = [params[name].init_value for name in names]
         final_values = [params[name].value for name in names]
-        ini, final = boot._initial_final_values(result.estimation_params,
-                                                names, only_vary)
+        ini, final = self.boot._initial_final_values(
+            self.result.estimation_params, names, only_vary)
+
         if only_vary:
             initial_values = initial_values[1:]
             final_values = final_values[1:]
+
         self.assertEqual(final, final_values)
         self.assertEqual(ini, initial_values)
 
@@ -89,17 +103,19 @@ class TestBootStrap(unittest.TestCase):
                            [8, 33, 'residues'],
                            [6, 10, 'data']])
     def test_generate_data_sets(self, n_boots, size, data_from):
-        boot_2 = BootStrap(result)
+        boot_2 = BootStrap(self.result)
         data2 = boot_2.generate_datasets(n_boots, size, data_from, True)
         self.assertEqual(data_from, boot_2.data_simulated)
         self.assertEqual(data2.shape[2], n_boots)
+
         if size == 'residues':
             self.assertEqual(data2._size, size)
 
     def test_fit_bootstrap(self):
-        boot_2 = BootStrap(result)
+        boot_2 = BootStrap(self.result)
         boot_2.generate_datasets(3, 25, 'data')
         boot_2.fit_bootstrap()
+
         for i in range(1, boot_2.bootstrap_result.shape[0]+1):
             val1 = round(boot_2.bootstrap_result['tau1 final'][i])
             val2 = round(boot_2.bootstrap_result['tau2 final'][i])
@@ -110,7 +126,8 @@ class TestBootStrap(unittest.TestCase):
             self.assertEqual(val2, 30)
             self.assertEqual(boot_2.bootstrap_result['tau3 initial'][i], 500)
             self.assertEqual(val3, 200)
-        self.assertEqual(data.shape[2], 3)
+
+        self.assertEqual(self.data.shape[2], 3)
         
         
 if __name__ == '__main__':
--- tests/fit/test_GlobalFitClass.py
+++ tests/fit/test_GlobalFitClass.py
@@ -5,19 +5,6 @@ from ultrafast.utils.divers import get_root_directory, read_data, select_traces
 import numpy as np
 import os
 
-def assertNearlyEqualArray(array1, array2, decimal):
-    """
-    returns "True" if all elements of two arrays
-    are identical until decimal
-    """
-    if type(array1) == list:
-        array1 = np.array(array1)
-    if type(array2) == list:
-        array2 = np.array(array2)
-    dif = np.array(array1) - np.array(array2)
-    value = (dif < 10**(-decimal)).all()
-    return value
-
 
 class TestGlobalFitClass(unittest.TestCase):
     @classmethod
@@ -25,13 +12,13 @@ class TestGlobalFitClass(unittest.TestCase):
         path = os.path.join(get_root_directory(), 'examples/data/denoised_2.csv')
         path2 = os.path.join(get_root_directory(), 'examples/data/gauss_denoised.csv')
 
-        self.original_taus = [8, 30, 200]
+        cls.original_taus = [8, 30, 200]
 
-        self.time, self.data, self.wave = read_data(path, wave_is_row=True)
-        self.data_select, self.wave_select = select_traces(self.data, self.wave, 'auto')
+        cls.time, cls.data, cls.wave = read_data(path, wave_is_row=True)
+        cls.data_select, cls.wave_select = select_traces(cls.data, cls.wave, 'auto')
 
-        self.time_gauss, self.data_gauss, self.wave_gauss = read_data(path2, wave_is_row=True)
-        self.data_select_gauss, self.wave_select_gauss = select_traces(self.data_gauss, self.wave_gauss, 10)
+        cls.time_gauss, cls.data_gauss, cls.wave_gauss = read_data(path2, wave_is_row=True)
+        cls.data_select_gauss, cls.wave_select_gauss = select_traces(cls.data_gauss, cls.wave_gauss, 10)
 
     def test_globalfit_exponential(self):
         result = globalfit_exponential(self.time, self.data_select, 4, 40, 400)
@@ -41,7 +28,7 @@ class TestGlobalFitClass(unittest.TestCase):
             params_result['tau2_1'].value,
             params_result['tau3_1'].value
         ]
-        self.assertTrue(assertNearlyEqualArray(self.original_taus, final_taus, 7))
+        self.assertTrue(self.assertNearlyEqualArray(self.original_taus, final_taus, 7))
 
     def test_globalfit_gauss_exponential(self):
         result = globalfit_gauss_exponential(
@@ -50,7 +37,21 @@ class TestGlobalFitClass(unittest.TestCase):
 
         params_result = result.params
         final_taus = [params_result['tau1_1'].value, params_result['tau2_1'].value, params_result['tau3_1'].value]
-        self.assertTrue(assertNearlyEqualArray(self.original_taus, final_taus, 3))
+        self.assertTrue(self.assertNearlyEqualArray(self.original_taus, final_taus, 3))
+
+    @staticmethod
+    def assertNearlyEqualArray(array1, array2, decimal):
+        """
+        returns "True" if all elements of two arrays
+        are identical until decimal
+        """
+        if type(array1) == list:
+            array1 = np.array(array1)
+        if type(array2) == list:
+            array2 = np.array(array2)
+        dif = np.array(array1) - np.array(array2)
+        value = (dif < 10**(-decimal)).all()
+        return value
 
 
 if __name__ == '__main__':
