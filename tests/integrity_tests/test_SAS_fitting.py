1985df584fb0f2b64de14f2b887148c8378f71c2
Stahux <sn@nizin.pl>
2021-11-12 21:16:18 +0100

    Some minor changes in test files, now it also test if DAS shapes are correct. Passed positively.

--- tests/integrity_tests/test_SAS_fitting.py
+++ tests/integrity_tests/test_SAS_fitting.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep 25 15:37:51 2021
+Created on Fri Nov 12 17:44:42 2021
 
 @author: staszek
 """
+
 
 import unittest
 from ultrafast.utils.divers import DataSetCreator
 from ultrafast.utils.divers import read_data, select_traces
 from ultrafast.experiment import Experiment
+from ultrafast.fit.targetmodel import ModelWindow, Model
 import numpy as np
 
 
@@ -25,7 +27,7 @@ class TestDatasetsDAS(unittest.TestCase):
     def setUp(self):
         self.datasets_dir = "../../examples/dynamically_created_data/"
     
-    def test_genAndFit3expNoNoiseDAS(self):
+    def test_genAndFit3expNoNoiseSequential(self):
         #generate and save dataset, then fit it and verify results
         
         taus = [5,20,100]
@@ -41,8 +43,8 @@ class TestDatasetsDAS(unittest.TestCase):
         k1=1/taus[0]
         k2=1/taus[1]
         k3=1/taus[2]
-        kmatrix = [[-k1,0,0],[0,-k2,0],[0,0,-k3]]
-        initials = [0.33,0.34,0.33]
+        kmatrix = [[-k1,0,0],[k1,-k2,0],[0,k2,-k3]]
+        initials = [1,0.0,0.0]
         profiles = DataSetCreator.generate_profiles(500.0,5000,
                                                     initials,kmatrix)
         data_set_conv = DataSetCreator.generate_dataset(shapes, 
@@ -54,7 +56,7 @@ class TestDatasetsDAS(unittest.TestCase):
         data_set_conv_proj = DataSetCreator.timegrid_projection(data_set_conv, 
                                                                 new_times)        
         
-        datapath = self.datasets_dir+"DAS_3exp_nonoise_test1.csv"
+        datapath = self.datasets_dir+"SAS_3exp_nonoise_sequential_test1.csv"
         
         data_set_conv_proj.to_csv(datapath)
         
@@ -62,7 +64,9 @@ class TestDatasetsDAS(unittest.TestCase):
         
         experiment.select_traces(points='all')
         
-        experiment.fitting.initialize_exp_params(0, 0.20, 3, 30, 300)
+        
+        
+        experiment.fitting.initialize_target_params(0, 0.20, model=1)
         
         experiment.fitting.fit_global()
 
@@ -86,68 +90,14 @@ class TestDatasetsDAS(unittest.TestCase):
                 preexp_out = experiment.fitting.fit_records.global_fits[1].params["pre_exp"+str(i+1)+"_"+str(i_wave+1)].stderr            
                 
                 gen_preexps_list = [shapes.iloc[i_t,i_wave] for i_t in range(len(taus))]
-                if(np.abs(preexp) < sum(np.abs(gen_preexps_list))/20):
+                if(np.abs(preexp) < sum(np.abs(gen_preexps_list))/10):
                     continue #just skip places where given component had no bands
                 
                 self.assertTrue(abs((preexp-preexp_out)/preexp_err) < 5,
                                 msg="""Preexp[%i,%i] generated is %.6f, preexp after fit is 
-                                %.6f, and error is %.6f""" % (i,i_wave,preexp,preexp_out,preexp_err))              
+                                %.6f, and error is %.6f""" % (i,i_wave,preexp,preexp_out,preexp_err))  
 
-    def test_genAndFit1expNoNoiseDAS(self):
-        #generate and save dataset, then fit it and verify results
-        
-        tau = 35.0
-        
-        wave = DataSetCreator.generate_wavelength(400,700,31)
-        peaks=[[460,560,660],]
-        amplitudes=[[-10,6,12],]
-        fwhms=[[50,100,80],]
-        shapes = DataSetCreator.generate_specific_shape(wave, taus=1, 
-                                                        peaks=peaks, 
-                                                        amplitudes=amplitudes, 
-                                                        fwhms=fwhms)
-        k1=1/tau
-        kmatrix = [[-k1,],]
-        initials = [1,]
-        profiles = DataSetCreator.generate_profiles(300.0,5000,
-                                                    initials,kmatrix)
-        data_set_conv = DataSetCreator.generate_dataset(shapes, 
-                                                        profiles, 
-                                                        0.1)
-        new_times = DataSetCreator.generate_time(data_set_conv.index[0],
-                                                 data_set_conv.index[-1]-0.01,
-                                                 120)
-        data_set_conv_proj = DataSetCreator.timegrid_projection(data_set_conv, 
-                                                                new_times)        
-        
-        datapath = self.datasets_dir+"DAS_1exp_nonoise_test1.csv"
-        
-        data_set_conv_proj.to_csv(datapath)
-        
-        experiment = Experiment.load_data(datapath, wave_is_row=True)
-        
-        experiment.select_traces(points='all')
-        
-        experiment.fitting.initialize_exp_params(0, 0.10, 70.0)
-        
-        experiment.fitting.fit_global()
 
-        tau_err = experiment.fitting.fit_records.global_fits[1].params["tau1_1"].value
-        tau_out = experiment.fitting.fit_records.global_fits[1].params["tau1_1"].stderr
-        
-         #test equality of taus and preexps within error range
-        self.assertTrue(abs((tau-tau_out)/tau_err) < 5,
-                        msg="""Tau generated is %.3f, tau after fit is 
-                        %.3f, and error is %.3f""" % (tau,tau_out,tau_err))       
-        
-        for i_wave in range(len(wave)):
-            preexp = shapes.iloc[0,i_wave]
-            preexp_err = experiment.fitting.fit_records.global_fits[1].params["pre_exp1_"+str(i_wave+1)].value
-            preexp_out = experiment.fitting.fit_records.global_fits[1].params["pre_exp1_"+str(i_wave+1)].stderr            
-            
-            self.assertTrue(abs((preexp-preexp_out)/preexp_err) < 5,
-                            msg="""Preexp generated is %.6f, preexp after fit is 
-                            %.6f, and error is %.6f""" % (preexp,preexp_out,preexp_err))             
 
 if __name__ == '__main__':
     unittest.main()
